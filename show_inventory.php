a6caace103d4dcc8229c2e9fa0e95f18e4e3fef6
Harold Hong <monkeyoohlala@outlook.com>
2025-07-06 10:11:24 -0700

    made list

--- show_inventory.php
+++ show_inventory.php
@@ -1,4 +1,10 @@
 <?php
+
+    /*
+        Harold Hong
+        https://github.com/monkeyoohlala/cs85-module4b-inventory
+    */
+
     try {
         $db = new PDO("mysql:host=localhost;dbname=inventory_db", "root", "");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -6,11 +12,19 @@
         $stmt = $db->query("SELECT * FROM items");
         $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        echo "<p>List of Items</p><br>";
+        echo "<ul>";
+
         foreach ($items as $item) {
-            echo "<p>{$item['item_name']} ({$item['quantity']} units)</p>";
+            echo "<li>{$item['item_name']} ({$item['quantity']} units)</li>";
         }
+
+        echo "</ul>";
+
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
 ?>
+  
+
   
